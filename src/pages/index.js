da800ca20ec014e1ffab6ccef10f11548f35d765
EgloDevelopment <UNUstifA4W346TcF2Qwc@gmail.com>
2024-06-13 08:53:58 -0600

    transfer

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,46 @@
+import React, { useState, useEffect } from "react";
+
+import Navbar from "@/components/Navbar";
+
 export default function Page() {
-  return (
-    <main>
-      <div>Teting</div>
-    </main>
-  );
+  const [heroText, setHeroText] = useState("")
+
+  useEffect(() => {
+    const textChangeInterval = setInterval(() => {
+      changeText()
+    }, 5000);
+
+    return () => clearInterval(textChangeInterval);
+  }, []);
+
+  async function changeText() {
+    let text_array = ["sync", "share", "save", "secure"]
+
+    Array.prototype.sample = function(){
+      return this[Math.floor(Math.random()*this.length)];
+    }
+
+    const chosen_text = text_array.sample()
+
+    setHeroText(chosen_text)
+  }
+
+	return (
+		<main>
+			<div className="sticky">
+				<Navbar className="sticky top-0" />
+			</div>
+			<div className="hero min-h-screen bg-base-100 -mt-10">
+				<div className="hero-content text-center">
+					<div className="max-w-md">
+						<h1 className="text-5xl font-bold">
+							A new way to <ul>sync</ul>
+						</h1>
+						<p className="py-6">Experience a level of inter-connected device functionality never before brought to such a wide ecosystem with such interactivity and robustness.</p>
+						<button className="btn btn-primary">Get Started</button>
+					</div>
+				</div>
+			</div>
+		</main>
+	);
 }
